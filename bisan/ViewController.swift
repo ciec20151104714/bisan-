80b50c63de258c43f63e7a1179f9ba63cd843013
s20151104714 <s20151104714@admindeiMac-62.local>
2017-04-26 10:52:58 +0800

    sadsadasd

---
+++ bisan/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  bisan
+//
+//  Created by s20151104714 on 2017/3/29.
+//  Copyright © 2017年 s20151104714. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var a=4
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
